5ad22aaf30ce4f3ab430b156dec5f7476be4dfd9
rarnecros <rarnecros306@gmail.com>
2024-12-15 19:48:56 +0500

    this is my cahi or code

---
+++ app.js
@@ -0,0 +1,64 @@
+// let name = 'israr'
+// let age = 18;
+// console.log(name)
+
+
+
+
+// js fundemantal
+
+// let 
+// const
+// var
+
+
+// data types
+
+// let name = "israr"
+// name = "kabeer"
+// console.log(name)
+
+
+// function name(){
+//     let num1 = 10;
+//     let num2 = 20;
+//     c = num1 * num2;
+//     console.log(c)
+// }
+// name()
+// name()
+// name()
+
+
+// class1 chai or code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
